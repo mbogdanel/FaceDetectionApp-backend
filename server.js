c3e8579d61de77d9a9468d128dba47ec5a081a49
Marius Bogdanel <marius.bogdanel@gmail.com>
2020-11-08 17:51:19 +0000

    fix

--- server.js
+++ server.js
@@ -13,8 +13,12 @@ const db = knex({
   // connect to your own database here
   client: 'pg',
   connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'smart-brain'
+    // connectionString: process.env.DATABASE_URL,
+    // ssl: true
   }
 });
 
@@ -31,5 +35,5 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3000, ()=> {
-  console.log(`app is running on port ${ process.env.PORT }`);
+  console.log(`app is running on port 3000 ${ process.env.PORT }`);
 })
