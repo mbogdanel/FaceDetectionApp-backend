1fdbbcece6728425a34be29e2418fd7c343454c4
Marius Bogdanel <marius.bogdanel@gmail.com>
2020-11-08 11:28:15 +0000

    fix

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  
+    ssl: true
   }
 });
 
